6dbf3f89a4690a00c08c0696d4b60ad16d89888c
imo-tikuwa <imotikuwabathclean@gmail.com>
2021-11-05 19:11:24 +0900

    サンプルコマンドを作成

---
+++ src/Providers/ConsoleServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace UcanLab\LaravelHelloWorld\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use ImoTikuwa\ModelGenerator\Console\Commands\SampleCommand;
+
+class ConsoleServiceProvider extends ServiceProvider
+{
+    /**
+     * Initial startup process for all application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                SampleCommand::class,
+            ]);
+        }
+    }
+}
